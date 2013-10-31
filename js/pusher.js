c34fc0de6ef50a61e84d6217614a6b021b1843b5
Jimmy Wennlund <jimmy@spotifybusiness.com>
2013-10-31 13:43:41 +0100

    created an id sequence for the client. renamed callbacks to subscriptions. added callbacks and errbacks to provide proper async response handling in the client. moved buffer flushing from onconnect to incoming Welcome messages. made pusher.send provide ids for all objects with id==true, and register callback/errback for all objects with callback or errback.

--- js/pusher.js
+++ js/pusher.js
@@ -1,5 +1,7 @@
 function Pusher(options) {
   var that = this;
+  // next message id
+  that.nextId = 0;
   // socket url
   that.url = options.url;
   // heartbeat interval
@@ -20,12 +22,25 @@ function Pusher(options) {
   that.reconnector = null;
   // buffer while closed
   that.buffer = [];
+  // subscriptions
+  that.subscriptions = {};
   // callbacks
   that.callbacks = {};
+  // errbacks
+  that.errbacks = {};
   // error handler
-  that.onerror = options.onerror || (function(err) { console.log('pusher error:', err); });
+  that.onerror = options.onerror || function(err) {
+    console.log('pusher error:', err);
+  };
   // on connect handler
-  that.onconnect = options.onconnect || (function(msg) { console.log('pusher connected:', msg); });
+  that.onconnect = options.onconnect || function(msg) {
+    console.log('pusher connected:', msg);
+  };
+  // authorizer is a function that generate authorization tokens for your rights.
+  that.authorizer = options.authorizer || function(uri, write) {
+    console.log('authorizer returned empty token for ', uri, write);
+    return "";
+  };
   /*
    * set up the socket
    */
@@ -38,9 +53,6 @@ function Pusher(options) {
     that.socket.onopen = function() {
       that.lastHeartbeatReceived = new Date();
       that.backoff = that.minBackoff;
-      while (that.buffer.length > 0) {
-        that.send(that.buffer.shift());
-      }
     };
     that.socket.onerror = function(err) {
       that.onerror(err);
@@ -59,11 +71,12 @@ function Pusher(options) {
   that.handleMessage = function(msg) {
     if (msg.Type == "Welcome") {
       that.heartbeat = msg.Welcome.Heartbeat;
-      if (msg.Welcome.Id != that.id) {
-        for (var uri in that.callbacks) {
+      if (that.id != null && msg.Welcome.Id != that.id) {
+        for (var uri in that.subscriptions) {
           that.send({
             Type: 'Subscribe',
-            URI: uri
+            URI: uri,
+            Id: true
           });
         }
       }
@@ -75,26 +88,45 @@ function Pusher(options) {
         if (new Date().getTime() - that.lastHeartbeatReceived.getTime() > that.heartbeat) {
           that.close()
         } else {
-          that.send({
-            Type: "Heartbeat"
-          });
+          if (that.socket.readyState == 1) {
+            that.send({
+              Type: "Heartbeat"
+            });
+          } 
         }
       }, that.heartbeat / 2);
+      while (that.buffer.length > 0) {
+        that.send(that.buffer.shift());
+      }
       that.onconnect(msg);
     } else if (msg.Type == "Heartbeat") {
       that.lastHeartbeatReceived = new Date();
     } else if (msg.Type == "Message") {
-      var callbacks = that.callbacks[msg.URI];
-      if (callbacks != null) {
-        for (var callback in callbacks) {
-          callbacks[callback].apply(msg, msg.Data);
+      var subscriptions = that.subscriptions[msg.URI];
+      if (subscriptions != null) {
+        for (var subscription in subscriptions) {
+          subscriptions[subscription].apply(msg, msg.Data);
         }
       }
     } else if (msg.Type == "Error") {
       if (msg.Data.Type == "Subscribe") {
-        delete(that.callbacks[msg.Data.URI]);
+        delete(that.subscriptions[msg.Data.URI]);
       }
-      that.onerror(msg);
+      var obj = that.errbacks[msg.Id];
+      if (obj != null) {
+        obj.errback.call(obj, msg);
+      } else {
+        that.onerror(msg);
+      }
+      delete(that.errbacks[msg.Id]);
+      delete(that.callbacks[msg.Id]);
+    } else if (msg.Type == "Ack") {
+      var obj = that.callbacks[msg.Id];
+      if (obj != null) {
+        obj.callback.call(obj, msg);
+      }
+      delete(that.callbacks[msg.Id]);
+      delete(that.errbacks[msg.Id]);
     } else {
       that.onerror({
         Type: "Error",
@@ -103,45 +135,40 @@ function Pusher(options) {
       });
     }
   };
-  that.authorize = function(uri, token, write) {
-    that.send({
-      Type: 'Authorize',
-      URI: uri,
-      Token: token,
-      Write: write
-    });
-  };
   that.emit = function(uri, data) {
     // Copy arguments, becouse slice changes it.
     var args = Array.prototype.slice.call(arguments, 0);
     that.send({
       Type: 'Message',
       URI: uri,
-      Data: args.slice(1)
+      Data: args.slice(1),
+      Id: true
     });
   };
-  that.on = function(uri, callback) {
-    if (that.callbacks[uri] == null) {
-      that.callbacks[uri] = {};
+  that.on = function(uri, subscription) {
+    if (that.subscriptions[uri] == null) {
+      that.subscriptions[uri] = {};
       that.send({
         Type: 'Subscribe',
-        URI: uri
+        URI: uri,
+        Id: true
       });
     }
-    that.callbacks[uri][callback] = callback;
+    that.subscriptions[uri][subscription] = subscription;
   };
-  that.off = function(uri, callback) {
-    delete(that.callbacks[uri][callback]);
+  that.off = function(uri, subscription) {
+    delete(that.subscriptions[uri][subscription]);
     var left = 0;
-    for (var callback in that.callbacks[uri]) {
+    for (var subscription in that.subscriptions[uri]) {
       left++;
     }
     if (left == 0) {
       that.send({
         Type: 'Unsubscribe',
-        URI: uri
+        URI: uri,
+        Id: true
       });
-      delete(that.callbacks[uri]);
+      delete(that.subscriptions[uri]);
     }
   };
   /*
@@ -164,6 +191,15 @@ function Pusher(options) {
    */
   that.send = function(obj) {
     if (that.socket.readyState == 1) {
+      if (obj.Id) {
+        obj.Id = that.id + ":" + that.nextId++;
+        if (obj.callback != null) {
+          that.callbacks[obj.Id] = obj;
+        }
+        if (obj.errback != null) {
+          that.errbacks[obj.Id] = obj;
+        }
+      }
       that.socket.send(JSON.stringify(obj));
     } else {
       that.buffer.push(obj);
